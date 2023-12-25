949b413e2b2fa47df69efd7dbe90d6b7a565b00b
JOHNNYLEEWORK <154789464+JOHNNYLEEWORK@users.noreply.github.com>
2023-12-26 03:40:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'JohnnyLee', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '3D 制作师', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://portfolio-omega-lovat-70.vercel.app', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://johnnylee.vercel.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
