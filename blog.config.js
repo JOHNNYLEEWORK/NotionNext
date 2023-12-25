a93cd5a2bca0ebb1aab1e421cc292f8570fe27d9
JOHNNYLEEWORK <154789464+JOHNNYLEEWORK@users.noreply.github.com>
2023-12-26 02:35:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,12 +19,12 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'JohnnyLee', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '3D 制作师', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://portfolio-omega-lovat-70.vercel.app/article/CV', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://portfolio-omega-lovat-70.vercel.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || 'https://portfolio-omega-lovat-70.vercel.app/article/CV', // 你的微博个人主页
+  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
